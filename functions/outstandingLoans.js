2074e5e41378041a607b286905f877c9d6d4b3a4
Aryan Sharma <aryansharma.prime@gmail.com>
2022-05-29 04:44:45 +0530

    loans

---
+++ functions/outstandingLoans.js
@@ -0,0 +1,37 @@
+const moment = require("moment");
+
+async function outstandingLoans(txns, data) {
+  const loansTaken = txns.txns.filter(
+    (txn) => txn[`input`].substring(0, 10) === "0xc5ebeaec"
+  );
+  const loansPaid = txns.txns.filter(
+    (txn) => txn[`input`].substring(0, 10) === "0x0e752702"
+  );
+
+  if (data.outstandingDays >= 1) {
+    const daysPaid = [];
+
+    loansTaken.map((loan) => {
+      const loanPaid = loansPaid.filter(
+        (paid) => paid[`from_address`] === loan[`to_address`]
+      );
+      if (loanPaid.length > 0) {
+        let loanPaidDate = moment(loanPaid[0][`block_timestamp`]);
+        let loanTakenDate = moment(loan[`block_timestamp`]);
+        daysPaid.push(loanPaidDate.diff(loanTakenDate, "days"));
+      }
+    });
+
+    const average =
+      daysPaid.reduce((partialSum, a) => partialSum + a, 0) / daysPaid.length;
+    return average;
+  } else {
+    if (loansTaken.length > loansPaid.length) {
+      return loansTaken.length - loansPaid.length;
+    } else {
+      return 0;
+    }
+  }
+}
+
+module.exports = outstandingLoans;

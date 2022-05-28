07f8bfcdb52b3cf70e4e3655cd8513c6489bc529
Aryan Sharma <aryansharma.prime@gmail.com>
2022-05-28 14:10:59 +0530

    ens

---
+++ functions/ens.js
@@ -0,0 +1,16 @@
+const Moralis = require("moralis/node");
+
+/* Moralis init code */
+const serverUrl = "https://s7xdxxyqs8ho.usemoralis.com:2053/server";
+const appId = "dNiFIgxMJYi7Tmj1JHtMd6YyS6vdAduPMy7sLZes";
+const masterKey = "3FiUlEpSuAVWUOO87r9AlZWIj2jh80a95sjOQAPS";
+
+async function getEns(address) {
+  await Moralis.start({ serverUrl, appId, masterKey });
+  const options = { address: address };
+  const resolve = await Moralis.Web3API.resolve.resolveAddress(options);
+  if (resolve.name !== "") {
+    return true;
+  }
+  return false;
+}
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });

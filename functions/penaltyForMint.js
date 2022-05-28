ab1a43179c31a7c37d3ae633870391a1dd7d1cfd
Aryan Sharma <aryansharma.prime@gmail.com>
2022-05-29 03:46:16 +0530

    token address

--- functions/penaltyForMint.js
+++ functions/penaltyForMint.js
@@ -1,3 +1,8 @@
+const Web3 = require("web3");
+const web3 = new Web3(
+  new Web3.providers.HttpProvider("6cNPtRjviUYaviwhTwXvFAAAwHpE4jWo")
+);
+
 async function penaltyForMint(txns, address, toOrFrom = "to") {
   const transactions = txns.txns;
   transactions.map((txn) => {
@@ -13,14 +18,16 @@ async function penaltyForMint(txns, address, toOrFrom = "to") {
           (txn) => txn.timestamp - timestamp < time_difference
         );
         const penaltyTransactions = [];
+        const token1 = await web3.eth.getTransaction(txn.hash)
+        const token2 = await web3.eth.getTransaction(txn.hash)
         if (mint_txn.length > 0) {
           mint.txn.map((t) => {
             // these methods are for transfer, uniswap multicall and approve
             if (
-              t.input == "0xa9059cbb" ||
+              (t.input == "0xa9059cbb" ||
               t.input == "0x5ae401dc" ||
               t.input == "0x095ea7b3"
-            ) {
+            ) && ("0x"+token1.substring(38, 78) == "0x"+token2.substring(38, 78))) {
               penaltyTransactions.push(t);
             }
           });

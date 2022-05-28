66769ed9f12b41dcd6dc9d063218b13219584dad
ayush <62694274+ayshptk@users.noreply.github.com>
2022-05-28 23:31:08 +0530

    fix request timeout

--- utils/findScores.js
+++ utils/findScores.js
@@ -2,11 +2,26 @@ const scores = require("../scores.json");
 
 function findScore(query, number) {
   const xyz = scores[query];
+  const minimum = xyz[0];
+  const multiplier = xyz[1];
   let score = 0;
   let cursor = 0;
-  let 
-  while (cursor < number) {
-    score += xyz[cursor];
-    cursor++;
+  let currentBase = minimum;
+  if (number < minimum) {
+    return 0;
   }
+  score = 1;
+  while (cursor < number) {
+    const requiredBase = multiplier * currentBase;
+    if (cursor + requiredBase > number) {
+      score += (number - cursor) / requiredBase;
+    } else {
+      score += 1;
+    }
+    cursor += requiredBase;
+    console.log(cursor, number);
+  }
+  return score;
 }
+
+module.exports = findScore;

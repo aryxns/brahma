e17e2c1513b127e31be2b0522a26c123b88861cf
Digvijay Singh Rathore <digvijaysra@gmail.com>
2022-05-29 05:36:01 +0530

    cast votes and stuff

--- functions/castVote.js
+++ functions/castVote.js
@@ -14,18 +14,19 @@ function getEtherscanApi() {
   ];
 }
 
-function getVoted(txns) {
+function getVoted(txns, needNumber) {
   let voted = 0;
   const transactions = txns.txns;
   transactions.map((tx) => {
-    if (voted == 1) {
+    if(!needNumber && voted == 1) {
       return voted;
-    } else {
-      if (tx.input.substring(0, 10) == "0x56781388") {
-        voted = 1;
-      }
     }
-  })
+    
+    if (tx.input.substring(0, 10) == "0x56781388") {
+      voted++;
+    }
+  });
+
   return voted;
 }
 
